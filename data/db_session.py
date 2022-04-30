273929d83f1ce38758cd581d882fddeaea697cc3
dimon-prog <51048599+dimon-prog@users.noreply.github.com>
2022-04-30 13:26:56 +0400

    Update db_session.py

--- data/db_session.py
+++ data/db_session.py
@@ -2,7 +2,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
-
+import os
 SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
@@ -16,7 +16,12 @@ def global_init(db_file):
 
     if not db_file or not db_file.strip():
         raise Exception("Необходимо указать файл базы данных.")
-
+    if 'DATABASE_URL' in os.environ:  # сработает на Heroku
+        conn_str = os.environ['DATABASE_URL'].replace('postgres://', 'postgresql://')
+    else:  # сработает локально
+        from config import LOCAL_DB
+        conn_str = LOCAL_DB
+    engine = sa.create_engine(conn_str, echo=False)
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
