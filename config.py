04e90aaf7abd00287fa23b4b8d20a2c09d1e05d6
dimon-prog <51048599+dimon-prog@users.noreply.github.com>
2022-04-30 13:24:29 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 API_TOKEN = '5153157850:AAHNPS6bMA8NPP-aMgaetq-X1NLjjJAjt6E'
 # bot token from @BotFather
+POSTGRES = "postgresql://icotbjclnxaxzn:a1406cf345b2e86bbeee2d22"
+
+DB_FILE = "/db/test.sqlite"  # если хотим оставить возможность переключиться обратно на sqlite
+SQLITE = f'sqlite://{DB_FILE}?check_same_thread=False'
+
+LOCAL_DB = POSTGRES
